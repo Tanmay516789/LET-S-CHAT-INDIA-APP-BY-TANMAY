6fbec651dac1e3dd0f6b07aa77f92ee0cd11b118
Tanmay516789 <96418540+Tanmay516789@users.noreply.github.com>
2022-01-07 06:19:49 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyBItT-0Tnwge1h39eqxaHmZZ4rEs_6cJKc",
+      authDomain: "kwitter-app-by-tanmay.firebaseapp.com",
+      databaseURL: "https://kwitter-app-by-tanmay-default-rtdb.firebaseio.com",
+      projectId: "kwitter-app-by-tanmay",
+      storageBucket: "kwitter-app-by-tanmay.appspot.com",
+      messagingSenderId: "916077784066",
+      appId: "1:916077784066:web:2dedf4315d7b805ad1cb1b",
+      measurementId: "G-9HCTBN5ZT9"
+    };
+  
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
